bac60913b23eab93087edf2fbed177527358ffe5
stuart.welham@gmail.com <stuart.welham@gmail.com>
2016-10-31 23:00:03 +0000

    initial tag parsing

--- lib/social_parser.ex
+++ lib/social_parser.ex
@@ -1,2 +1,24 @@
 defmodule SocialParser do
+  def parse_hashtags(message) do
+    message
+      |> parse([])
+      |> Enum.reverse()
+      |> to_string()
+      |> String.split()
+  end
+
+  defp parse(<<>>, acc), do: acc
+  defp parse(<<?#::utf8, rest::binary>>, acc) do
+    parse_hashtag(rest, [?#] ++ acc)
+  end
+  defp parse(<<c::utf8, rest::binary>>, acc) do
+      parse(rest, acc)
+  end
+
+  defp parse_hashtag(<<?\s::utf8, rest::binary>>, acc) do
+    parse(rest, [?\s] ++ acc)
+  end
+  defp parse_hashtag(<<c::utf8, rest::binary>>, acc) do
+      parse_hashtag(rest, [c] ++ acc)
+  end
 end
--- test/social_parser_test.exs
+++ test/social_parser_test.exs
@@ -2,7 +2,16 @@ defmodule SocialParserTest do
   use ExUnit.Case
   doctest SocialParser
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "parse_hashtags should return an empty list for blank input" do
+      tags = SocialParser.parse_hashtags("")
+
+      assert tags == []
+  end
+
+  test "parse_hashtags should return a list of found hashtags" do
+    message = "this is a #test #message with #a few #test tags!"
+    tags = SocialParser.parse_hashtags(message)
+
+    assert tags == ["#test", "#message", "#a", "#test"]
   end
 end
