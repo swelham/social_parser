b0101031f1d0089fd225fbc0b4c62fe9ff489051
stuart.welham@gmail.com <stuart.welham@gmail.com>
2016-11-03 21:08:48 +0000

    tidy up tests

--- test/social_parser_test.exs
+++ test/social_parser_test.exs
@@ -2,6 +2,8 @@ defmodule SocialParserTest do
   use ExUnit.Case
   doctest SocialParser
 
+  @test_message "@you this is a #test #message with #a few #test tags from +me"
+
   test "parse_hashtags should return an empty list for blank input" do
       tags = SocialParser.parse_hashtags("")
 
@@ -9,8 +11,7 @@ defmodule SocialParserTest do
   end
 
   test "parse_hashtags should return a list of found hashtags" do
-    message = "@you this is a #test #message with #a few #test tags!"
-    tags = SocialParser.parse_hashtags(message)
+    tags = SocialParser.parse_hashtags(@test_message)
 
     assert tags == %{tags: ["#test", "#message", "#a", "#test"]}
   end
@@ -22,18 +23,10 @@ defmodule SocialParserTest do
     assert tags == %{tags: ["#test", "#message"]}
   end
 
-  test "parse_mentions should return a list of found mentions using @" do
-    message = "this is @someones #test message from @no_one"
-    mentions = SocialParser.parse_mentions(message)
+  test "parse_mentions should return a list of found mentions" do
+    mentions = SocialParser.parse_mentions(@test_message)
 
-    assert mentions == %{mentions: ["@someones", "@no_one"]}
-  end
-
-  test "parse_mentions should return a list of found mentions using +" do
-    message = "this is +someones #test message from +no_one"
-    mentions = SocialParser.parse_mentions(message)
-
-    assert mentions == %{mentions: ["+someones", "+no_one"]}
+    assert mentions == %{mentions: ["@you", "+me"]}
   end
 
   test "parse_hashtags should separate joined mentions" do
